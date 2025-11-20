70e01820d8b9366f17a0de271054764d3e81fa12
malua <malua@users.noreply.github.com>
2025-11-20 18:45:59 +0100

    refactor: tag application-profile with type:domain and move it to grouping folder

--- libs/domain/application-profile/jest.config.ts
+++ libs/domain/application-profile/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'application-profile',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   globals: {},
   testEnvironment: 'node',
   transform: {
@@ -13,5 +13,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/application-profile',
+  coverageDirectory: '../../../coverage/libs/domain/application-profile',
 };
